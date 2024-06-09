16b306c5471a38a2d48e23a3bc0c5b09b48d930d
MinKhantKyawSwar <mkks1986.mkks@gmail.com>
2024-06-09 15:35:14 +0630

    mongodb

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,25 @@
 const mongodb = require("mongodb");
 const mongodbClient = mongodb.MongoClient;
-const dotenv = require("dotenv");
-dotenv.config();
+const dotenv = require("dotenv").config();
+
+let db;
 
 const mongodbConnector = () => {
   mongodbClient
     .connect(process.env.MONGODB_URL)
     .then((result) => {
       console.log("connected to database");
+      db = result.db();
       console.log(result);
     })
     .catch((err) => console.log(err));
 };
 
-module.exports = mongodbConnector;
+const getDatabase = () => {
+  if (db) {
+    return db;
+  }
+  throw "No Database";
+};
+
+module.exports = { mongodbConnector, getDatabase };
